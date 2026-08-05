4bfffd4657853af9380d083c20ba58e4f91056cc
sterbalibor-netizen <sterbalibor@gmail.com>
2026-08-05 15:55:12 +0200

    Add async function to load article content

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+async function nactiClanek(soubor) {
+    const odpoved = await fetch(soubor);
+    const html = await odpoved.text();
+    document.getElementById("hlavni-obsah").innerHTML = html;
+}
